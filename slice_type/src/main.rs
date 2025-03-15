7690f9690554a8b6cfe5f5957508aa5cfe444c79
Aradhya Nepal <aradhya.1221@gmail.com>
2025-03-15 18:06:50 +0545

    CHanges

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -1,20 +1,24 @@
 use std::io;
-fn main() {
-    println!("Enter an value");
-    let mut input=String::new();
-    io::stdin().read_line(&mut input).expect("Input");
-    let index_of_space=first_word(&input);
-    println!("Index of word is {index_of_space}");
-    input.clear();
-  
+
+fn main(){
+    let s=String::from("Hello World");
+    let hello=&s[..5];
+    let world=&s[6..];
+   println!("{hello} {world}");
+   let mut input=String::new();
+   io::stdin().read_line(&mut input).expect("Input");
+   let value=slice_the_value(&input);
+   input.clear();
+   println!("value is {value}");
 }
 
-fn first_word(value: &String)->usize{
- let bytes=value.as_bytes();
- for (i,&e) in bytes.iter().enumerate(){
-     if e == b' '{
-         return i;
-     }
- }
- value.len()
+
+fn slice_the_value(value:&String)->&str{
+   let binaries=value.as_bytes();
+   for (i,&e) in binaries.iter().enumerate(){
+       if e==b' '{
+           return &value[0..i];
+       }
+   }
+   &value[..]
 }
